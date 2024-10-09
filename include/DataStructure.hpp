fee3699d9aaca4a7ac25ab31e840345f81bfe894
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-10-09 14:33:35 +0200

    gpu trial started

--- include/DataStructure.hpp
+++ include/DataStructure.hpp
@@ -2,9 +2,9 @@
 
 #include <iostream>
 #include <vector>
-//#include <thrust/host_vector.h>
-//#include <thrust/device_vector.h>
-//#include <thrust/copy.h>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/copy.h>
 
 /**
  * @brief General 2D data structure around std::vector, in column
@@ -24,17 +24,8 @@ class Matrix {
    * @param[in] initial value for the elements
    *
    */
-  // Matrix<T>(int i_max, int j_max, double init_val)
-  //     : _imax(i_max), _jmax(j_max) {
-  //   _container.resize(i_max * j_max);
-  //   std::fill(_container.begin(), _container.end(), init_val);
-  // }
-  Matrix<T>(int i_max, int j_max, double init_val);
-  //     : _imax(i_max), _jmax(j_max) {
-  //   _container.resize(i_max * j_max);
-  //   std::fill(_container.begin(), _container.end(), init_val);
-  // }
-
+    Matrix<T>(int i_max, int j_max, double init_val);
+  
   /**
    * @brief Constructor without an initial value.
    *
@@ -43,56 +34,48 @@ class Matrix {
    *
    */
   Matrix<T>(int i_max, int j_max);
-  //  : _imax(i_max), _jmax(j_max) {
-  //   _container.resize(i_max * j_max);
-  // }
 
-  // /**
-  //  * @brief Element access and modify using index
-  //  *
-  //  * @param[in] x index
-  //  * @param[in] y index
-  //  * @param[out] reference to the value
-  //  */
+  /**
+   * @brief Element access and modify using index
+   *
+   * @param[in] x index
+   * @param[in] y index
+   * @param[out] reference to the value
+   */
   T &operator()(int i, int j); //{ return _container.at(_imax * j + i); }
 
-  // /**
-  //  * @brief Element access using index
-  //  *
-  //  * @param[in] x index
-  //  * @param[in] y index
-  //  * @param[out] value of the element
-  //  */
+  /**
+   * @brief Element access using index
+   *
+   * @param[in] x index
+   * @param[in] y index
+   * @param[out] value of the element
+   */
   T operator()(int i, int j) const;// { return _container.at(_imax * j + i); }
 
-  // /// get the number of elements in x direction
-  // int imax() const { return _imax; }
+  void printField() {
+    for(int j = 0; j < _jmax; j++) {
+      for(int i = 0; i < _imax; i++) {
+        std::cout << this->operator()(i,j) << " ";
+      }
+      std::cout << "\n";
+    }
+  }
 
-  // /// get the number of elements in y direction
-  // int jmax() const { return _jmax; }
-
-  // void printField() {
-  //   for(int j = 0; j < _jmax; j++) {
-  //     for(int i = 0; i < _imax; i++) {
-  //       std::cout << this->operator()(i,j) << " ";
-  //     }
-  //     std::cout << "\n";
-  //   }
-  // }
   void copyToDevice();
 
-//  thrust::host_vector<T> h_container;
-//  thrust::device_vector<T> d_container;
+  thrust::host_vector<T> h_container;
+  thrust::device_vector<T> d_container;
 
  private:
-  /// Number of elements in x direction
+  // Number of elements in x direction
   int _imax;
-  /// Number of elements in y direction
+
+  // Number of elements in y direction
   int _jmax;
 
-  /// Data container
+  // Data container
   std::vector<T> _container;
-  
 };
 
 // template class Matrix<float>;
