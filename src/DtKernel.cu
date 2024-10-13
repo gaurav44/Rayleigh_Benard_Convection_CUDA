d5d324c9a19492a288b8cebbf7a2ff1208f3ed4e
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-10-13 23:02:14 +0200

    Dt kernel with shared memory

--- src/DtKernel.cu
+++ src/DtKernel.cu
@@ -3,42 +3,33 @@
 #include <iterator>
 #include <thrust/device_vector.h>
 
-__global__ void Max_kernel_call(const double *U, const double *V, int imax,
-                                double jmax, double *u_max,
-                                double *v_max) { // double *max_results) {
+__global__ void UMax_kernel_call(const double *U, int imax, double jmax,
+                                 double *max_results) {
   int i = blockIdx.x * blockDim.x + threadIdx.x;
   int j = blockIdx.y * blockDim.y + threadIdx.y;
 
-  // extern __shared__ double shared_data[];
+  extern __shared__ double shared_data[];
+  int shared_index = threadIdx.x * blockDim.y + threadIdx.y;
+  shared_data[shared_index] = 0.0;
+
   if (i > 0 && j > 0 && i < imax - 1 && j < jmax - 1) {
-    int idx = j * imax + i;
-    if (i == 1 && j == 1) {
-      for (int ii = 1; ii < imax - 1; ii++) {
-        for (int jj = 1; jj < jmax - 1; jj++) {
-          *u_max = fmax(*u_max, fabs(U[idx]));
-          *v_max = fmax(*v_max, fabs(V[idx]));
-        }
+    int idx = imax * j + i;
+
+    shared_data[shared_index] = fabs(U[idx]);
+    __syncthreads(); // Synchronize to ensure all data is loaded
+
+    // Perform reduction in shared memory
+    for (int s = (blockDim.x * blockDim.y) / 2; s > 0; s /= 2) {
+      if (shared_index < s) {
+        shared_data[shared_index] =
+            fmax(shared_data[shared_index], shared_data[shared_index + s]);
       }
+      __syncthreads(); // Synchronize after each reduction step
     }
-    //  int idx = imax * j + i;
-
-    //  int shared_index = threadIdx.x * blockDim.y + threadIdx.y;
-    //  shared_data[shared_index] = 0.0;
-    //  shared_data[shared_index] = fabs(U[idx]);
-    //  __syncthreads(); // Synchronize to ensure all data is loaded
-
-    //  // Perform reduction in shared memory
-    //  for (int s = (blockDim.x * blockDim.y) / 2; s > 0; s /= 2) {
-    //    if (shared_index < s) {
-    //      shared_data[shared_index] =
-    //          fmax(shared_data[shared_index], shared_data[shared_index + s]);
-    //    }
-    //    __syncthreads(); // Synchronize after each reduction step
-    //  }
-    //}
-    //// Write the result of this block's max to global memory
-    // if (threadIdx.x == 0 && threadIdx.y == 0) {
-    //   max_results[blockIdx.x * gridDim.y + blockIdx.y] = shared_data[0];
+  }
+  // Write the result of this block's max to global memory
+  if (threadIdx.x == 0 && threadIdx.y == 0) {
+    max_results[blockIdx.x * gridDim.y + blockIdx.y] = shared_data[0];
   }
 }
 
@@ -48,11 +39,12 @@ __global__ void VMax_kernel_call(const double *V, int imax, double jmax,
   int j = blockIdx.y * blockDim.y + threadIdx.y;
 
   extern __shared__ double shared_data[];
+  int shared_index = threadIdx.x * blockDim.y + threadIdx.y;
+  shared_data[shared_index] = 0.0;
+
   if (i > 0 && j > 0 && i < imax - 1 && j < jmax - 1) {
     int idx = imax * j + i;
 
-    int shared_index = threadIdx.x * blockDim.y + threadIdx.y;
-    shared_data[shared_index] = 0.0;
     shared_data[shared_index] = fabs(V[idx]);
     __syncthreads(); // Synchronize to ensure all data is loaded
 
@@ -80,43 +72,39 @@ std::pair<double, double> Dt_kernel(const Matrix &U, const Matrix &V,
   double h_u_max = 0.0;
   double h_v_max = 0.0;
 
-  double *d_u_max;
-  double *d_v_max;
-  cudaMalloc(&d_u_max, sizeof(double));
-  cudaMalloc(&d_v_max, sizeof(double));
+  double *d_u_block_results;
+  double *d_v_block_results;
 
-  cudaMemcpy(d_u_max, &h_u_max, sizeof(double), cudaMemcpyHostToDevice);
-  cudaMemcpy(d_v_max, &h_v_max, sizeof(double), cudaMemcpyHostToDevice);
+  cudaMalloc(&d_u_block_results, numBlocks.x * numBlocks.y * sizeof(double));
+  cudaMalloc(&d_v_block_results, numBlocks.x * numBlocks.y * sizeof(double));
 
-  // double *d_u_block_results;
-  // double *d_v_block_results;
+  UMax_kernel_call<<<numBlocks, threadsPerBlock, 256 * sizeof(double)>>>(
+      thrust::raw_pointer_cast(U.d_container.data()), domain.imax + 2,
+      domain.jmax + 2, d_u_block_results);
 
-  // cudaMalloc(&d_u_block_results, 256 * sizeof(double));
-  // cudaMalloc(&d_v_block_results, 256 * sizeof(double));
-
-  Max_kernel_call<<<numBlocks, threadsPerBlock, 256 * sizeof(double)>>>(
-      thrust::raw_pointer_cast(U.d_container.data()),
+  VMax_kernel_call<<<numBlocks, threadsPerBlock, 256 * sizeof(double)>>>(
       thrust::raw_pointer_cast(V.d_container.data()), domain.imax + 2,
-      domain.jmax + 2, d_u_max, d_v_max);
-  // VMax_kernel_call<<<numBlocks, threadsPerBlock, 256 * sizeof(double)>>>(
-  //     d_V, domain.imax + 2, domain.jmax + 2, d_v_block_results);
-  // cudaDeviceSynchronize();
-  cudaMemcpy(&h_u_max, d_u_max, sizeof(double), cudaMemcpyDeviceToHost);
-  cudaMemcpy(&h_v_max, d_v_max, sizeof(double), cudaMemcpyDeviceToHost);
-  cudaFree(d_u_max);
-  cudaFree(d_v_max);
-  // double *h_umax_results = new double[256];
-  // double *h_vmax_results = new double[256];
-  // cudaMemcpy(h_umax_results, d_u_block_results, 256 * sizeof(double),
-  //            cudaMemcpyDeviceToHost);
-  // cudaMemcpy(h_vmax_results, d_v_block_results, 256 * sizeof(double),
-  //            cudaMemcpyDeviceToHost);
+      domain.jmax + 2, d_v_block_results);
+
+  cudaDeviceSynchronize();
+
+  double *h_umax_results = new double[numBlocks.x * numBlocks.y];
+  double *h_vmax_results = new double[numBlocks.x * numBlocks.y];
+  cudaMemcpy(h_umax_results, d_u_block_results,
+             numBlocks.x * numBlocks.y * sizeof(double),
+             cudaMemcpyDeviceToHost);
+  cudaMemcpy(h_vmax_results, d_v_block_results,
+             numBlocks.x * numBlocks.y * sizeof(double),
+             cudaMemcpyDeviceToHost);
+  cudaFree(d_u_block_results);
+  cudaFree(d_v_block_results);
 
   // Find the maximum in the result array
-  // for (int i = 0; i < 256; ++i) {
-  //  h_u_max = fmax(h_u_max, h_umax_results[i]);
-  //  h_v_max = fmax(h_v_max, h_vmax_results[i]);
-  //}
-
+  for (int i = 0; i < numBlocks.x * numBlocks.y; ++i) {
+    h_u_max = fmax(h_u_max, h_umax_results[i]);
+    h_v_max = fmax(h_v_max, h_vmax_results[i]);
+  }
+  free(h_umax_results);
+  free(h_vmax_results);
   return {h_u_max, h_v_max};
 }
