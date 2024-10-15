03906da87d79b33b741a0fa408cc5bc05942ff6a
Gaurav Gokhale <gauravgokhale752@gmail.com>
2024-10-15 17:17:31 +0200

    Removed temporary T_old buffer

--- src/TemperatureKernel.cu
+++ src/TemperatureKernel.cu
@@ -22,10 +22,10 @@ __global__ void temperature_kernel_call(const double *U, const double *V,
 }
 
 __global__ void temperature_kernelShared_call(const double *U, const double *V,
-                                              double *T, double *T_old,
-                                              double dx, double dy, int imax,
-                                              double jmax, double gamma,
-                                              double alpha, double dt) {
+                                              double *T, double dx, double dy,
+                                              int imax, double jmax,
+                                              double gamma, double alpha,
+                                              double dt) {
   // indices offset by 1 to account for halos
   int i = blockIdx.x * blockDim.x + threadIdx.x + 1;
   int j = blockIdx.y * blockDim.y + threadIdx.y + 1;
@@ -46,7 +46,7 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
   if (local_i > 0 && local_j > 0 && local_i < blockDim.x + 1 &&
       local_j < blockDim.y + 1) {
     shared_T[local_idx] = T[global_idx];
-    shared_Told[local_idx] = T_old[global_idx];
+    shared_Told[local_idx] = T[global_idx];
     shared_U[local_idx] = U[global_idx];
     shared_V[local_idx] = V[global_idx];
   }
@@ -54,7 +54,7 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
   // Left Halo
   if (threadIdx.x == 0 && i > 0) {
     shared_T[local_idx - 1] = T[global_idx - 1];
-    shared_Told[local_idx - 1] = T_old[global_idx - 1];
+    shared_Told[local_idx - 1] = T[global_idx - 1];
     shared_U[local_idx - 1] = U[global_idx - 1];
     shared_V[local_idx - 1] = V[global_idx - 1];
   }
@@ -62,7 +62,7 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
   // Right Halo
   if (threadIdx.x == blockDim.x - 1 && i < imax - 1) {
     shared_T[local_idx + 1] = T[global_idx + 1];
-    shared_Told[local_idx + 1] = T_old[global_idx + 1];
+    shared_Told[local_idx + 1] = T[global_idx + 1];
     shared_U[local_idx + 1] = U[global_idx + 1];
     shared_V[local_idx + 1] = V[global_idx + 1];
   }
@@ -70,7 +70,7 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
   // Bottom Halo
   if (threadIdx.y == 0 && j > 0) {
     shared_T[local_idx - blockDim.x - 2] = T[global_idx - imax];
-    shared_Told[local_idx - blockDim.x - 2] = T_old[global_idx - imax];
+    shared_Told[local_idx - blockDim.x - 2] = T[global_idx - imax];
     shared_U[local_idx - blockDim.x - 2] = U[global_idx - imax];
     shared_V[local_idx - blockDim.x - 2] = V[global_idx - imax];
   }
@@ -78,7 +78,7 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
   // Top Halo
   if (threadIdx.y == blockDim.y - 1 && j < jmax - 1) {
     shared_T[local_idx + blockDim.x + 2] = T[global_idx + imax];
-    shared_Told[local_idx + blockDim.x + 2] = T_old[global_idx + imax];
+    shared_Told[local_idx + blockDim.x + 2] = T[global_idx + imax];
     shared_U[local_idx + blockDim.x + 2] = U[global_idx + imax];
     shared_V[local_idx + blockDim.x + 2] = V[global_idx + imax];
   }
@@ -90,21 +90,19 @@ __global__ void temperature_kernelShared_call(const double *U, const double *V,
         shared_Told[local_idx] +
         dt * (alpha * Discretization::diffusionSharedMem(
                           shared_Told, local_i, local_j, blockDim.x + 2) -
-              Discretization::convection_TSharedMem(shared_U, shared_V, shared_Told, local_i, local_j, blockDim.x+2));
+              Discretization::convection_TSharedMem(shared_U, shared_V,
+                                                    shared_Told, local_i,
+                                                    local_j, blockDim.x + 2));
     T[global_idx] = shared_T[local_idx];
-    //T_old[global_idx] = shared_T[local_idx];
   }
 }
 
-void temperature_kernel(const Matrix &U, const Matrix &V, Matrix &T, 
+void temperature_kernel(const Matrix &U, const Matrix &V, Matrix &T,
                         const Domain &domain) {
 
   dim3 threadsPerBlock(16, 16);
   dim3 numBlocks((domain.imax + 2 + threadsPerBlock.x - 1) / threadsPerBlock.x,
                  (domain.jmax + 2 + threadsPerBlock.y - 1) / threadsPerBlock.y);
-
-  thrust::device_vector<double> T_old(T.d_container.size());
-  thrust::copy(T.d_container.begin(), T.d_container.end(), T_old.begin());
 
   size_t shared_mem =
       (threadsPerBlock.x + 2) * (threadsPerBlock.y + 2) * 4 * sizeof(double);
@@ -113,7 +111,7 @@ void temperature_kernel(const Matrix &U, const Matrix &V, Matrix &T,
       thrust::raw_pointer_cast(U.d_container.data()),
       thrust::raw_pointer_cast(V.d_container.data()),
       thrust::raw_pointer_cast(T.d_container.data()),
-      thrust::raw_pointer_cast(T_old.data()), domain.dx, domain.dy,
+      domain.dx, domain.dy,
       domain.imax + 2, domain.jmax + 2, domain.gamma, domain.alpha, domain.dt);
 
   CHECK(cudaGetLastError());
